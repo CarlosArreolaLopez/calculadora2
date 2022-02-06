bb2bd30636182eb0de462dcf16ba44240d5c5f46
juank <carlos.arreola@hotmail.com>
2022-02-06 13:47:10 -0600

    agregamos  web service guatemala queda pendiente practica

---
+++ Form2.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace calculadora2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgVariables = new System.Windows.Forms.DataGridView();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgVariables)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgVariables
+            // 
+            this.dgVariables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgVariables.Location = new System.Drawing.Point(12, 170);
+            this.dgVariables.Name = "dgVariables";
+            this.dgVariables.Size = new System.Drawing.Size(676, 268);
+            this.dgVariables.TabIndex = 0;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(558, 85);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(130, 56);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Listar Variables";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(122, 85);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(35, 13);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "label1";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.dgVariables);
+            this.Name = "Form2";
+            this.Text = "casa cambio guatemala";
+            ((System.ComponentModel.ISupportInitialize)(this.dgVariables)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgVariables;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
